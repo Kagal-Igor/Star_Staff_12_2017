73641eb598309099febfaec83fae9087ae8b093c
Kagal-Igor <cross.igor@yandex.ru>
2017-12-09 21:14:42 +0300

    Init

---
+++ SS_1Tests/SqTri90dgrTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Star_staff_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_staff_1.Tests
+{
+    [TestClass()]
+    public class SqTri90dgrTests
+    {
+        [TestMethod()]
+        public void countSTest_345()
+        {
+            Assert.AreEqual(6.0, SqTri90dgr.countS(3, 4, 5));
+            Assert.AreEqual(6.0, SqTri90dgr.countS(3, 4f, 5.0));
+            Assert.AreEqual(30.0, SqTri90dgr.countS(13, 12, 5));
+        }
+
+        [TestMethod()]
+        public void countSTest_m145()
+        {
+            Assert.AreEqual(null, SqTri90dgr.countS(-1, 4, 5));
+        }
+
+        [TestMethod()]
+        public void countSTest_minus145()
+        {
+            Assert.AreEqual(null, SqTri90dgr.countS(-1, 4, 5));
+        }
+
+        [TestMethod()]
+        public void countSTest_stringA45()
+        {
+            Assert.AreEqual(null, SqTri90dgr.countS("A", 4, 5));
+        }
+
+        [TestMethod()]
+        public void countSTest_string345()
+        {
+            Assert.AreEqual(6.0, SqTri90dgr.countS("3", 4, 5));
+        }
+        [TestMethod()]
+        public void countSTest_245()
+        {
+            Assert.AreEqual(null, SqTri90dgr.countS(2, 4, 5));
+        }
+    }
+}
---
+++ SqTri90dgr.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Star_staff_1
+{
+    public static class SqTri90dgr
+    {
+        public static object countS(object a, object b, object c)
+        {
+            try
+            {
+                double A = Convert.ToDouble(a);
+                double B = Convert.ToDouble(b);
+                double C = Convert.ToDouble(c);
+
+                if (A > 0 && B > 0 && C > 0)
+                {
+                    if (A * A == B * B + C * C)
+                        return B * C / 2.0;
+
+                    if (B * B == A * A + C * C)
+                        return A * C / 2.0;
+
+                    if (C * C == A * A + B * B)
+                        return A * B / 2.0;
+                }
+
+            }
+            catch { };
+
+            return null;
+        }
+    }
+}
